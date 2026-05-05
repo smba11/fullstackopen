fb2a23a3615067f5f2daae509c0bc4d4562678e2
smba11 <simba.1w34@gmail.com>
2026-05-05 10:21:32 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+const Header = (props) => {
+  return <h1>{props.course.name}</h1>
+}
+
+const Part = (props) => {
+  return <p>{props.part.name} {props.part.exercises}</p>
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map(part => 
+        <Part key={part.name} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p>Total of {total} exercises</p>
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
