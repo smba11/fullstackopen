269b662c6ed362e582be5122ed005322702833d3
smba11 <simba.1w34@gmail.com>
2026-05-16 23:07:50 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,116 @@
+import { useState } from 'react'
+
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={handleFilterChange} />
+    </div>
+  )
+}
+
+const PersonForm = ({
+  addPerson,
+  newName,
+  handleNameChange,
+  newNumber,
+  handleNumberChange
+}) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name:
+        <input value={newName} onChange={handleNameChange} />
+      </div>
+
+      <div>
+        number:
+        <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      {persons.map(person => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ))}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' }
+  ])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const nameExists = persons.some(
+      person => person.name === newName
+    )
+
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter
+        filter={filter}
+        handleFilterChange={(event) =>
+          setFilter(event.target.value)
+        }
+      />
+
+      <h2>Add a new</h2>
+
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={(event) =>
+          setNewName(event.target.value)
+        }
+        newNumber={newNumber}
+        handleNumberChange={(event) =>
+          setNewNumber(event.target.value)
+        }
+      />
+
+      <h2>Numbers</h2>
+
+      <Persons persons={personsToShow} />
+    </div>
+  )
+}
+
+export default App
