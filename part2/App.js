1d6c42b85cd7f0d9ed10242d2efd54015c2978ae
smba11 <simba.1w34@gmail.com>
2026-05-16 23:23:01 -0700

    Add files via upload

---
+++ part2/App.js
@@ -0,0 +1,219 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div style={{
+      color: 'green',
+      background: 'lightgrey',
+      fontSize: 20,
+      borderStyle: 'solid',
+      borderRadius: 5,
+      padding: 10,
+      marginBottom: 10
+    }}>
+      {message}
+    </div>
+  )
+}
+
+const Filter = ({ filter, handleFilterChange }) => {
+  return (
+    <div>
+      filter shown with <input value={filter} onChange={handleFilterChange} />
+    </div>
+  )
+}
+
+const PersonForm = ({
+  addPerson,
+  newName,
+  handleNameChange,
+  newNumber,
+  handleNumberChange
+}) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name:
+        <input value={newName} onChange={handleNameChange} />
+      </div>
+
+      <div>
+        number:
+        <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({ persons, deletePerson }) => {
+  return (
+    <div>
+      {persons.map(person => (
+        <p key={person.id}>
+          {person.name} {person.number}
+
+          <button onClick={() => deletePerson(person.id, person.name)}>
+            delete
+          </button>
+        </p>
+      ))}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const [message, setMessage] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const existingPerson = persons.find(
+      person => person.name === newName
+    )
+
+    if (existingPerson) {
+      const confirmUpdate = window.confirm(
+        `${newName} is already added to phonebook, replace the old number with a new one?`
+      )
+
+      if (confirmUpdate) {
+        const updatedPerson = {
+          ...existingPerson,
+          number: newNumber
+        }
+
+        axios
+          .put(
+            `http://localhost:3001/persons/${existingPerson.id}`,
+            updatedPerson
+          )
+          .then(response => {
+            setPersons(
+              persons.map(person =>
+                person.id !== existingPerson.id
+                  ? person
+                  : response.data
+              )
+            )
+
+            setMessage(
+              `Updated ${response.data.name}`
+            )
+
+            setTimeout(() => {
+              setMessage(null)
+            }, 5000)
+
+            setNewName('')
+            setNewNumber('')
+          })
+      }
+
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    axios
+      .post('http://localhost:3001/persons', personObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+
+        setMessage(
+          `Added ${response.data.name}`
+        )
+
+        setTimeout(() => {
+          setMessage(null)
+        }, 5000)
+
+        setNewName('')
+        setNewNumber('')
+      })
+  }
+
+  const deletePerson = (id, name) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      axios
+        .delete(`http://localhost:3001/persons/${id}`)
+        .then(() => {
+          setPersons(
+            persons.filter(person => person.id !== id)
+          )
+
+          setMessage(`Deleted ${name}`)
+
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+        })
+    }
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Notification message={message} />
+
+      <Filter
+        filter={filter}
+        handleFilterChange={(event) =>
+          setFilter(event.target.value)
+        }
+      />
+
+      <h2>Add a new</h2>
+
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={(event) =>
+          setNewName(event.target.value)
+        }
+        newNumber={newNumber}
+        handleNumberChange={(event) =>
+          setNewNumber(event.target.value)
+        }
+      />
+
+      <h2>Numbers</h2>
+
+      <Persons
+        persons={personsToShow}
+        deletePerson={deletePerson}
+      />
+    </div>
+  )
+}
+
+export default App
